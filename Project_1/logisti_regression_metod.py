fd4804cd2db5b22b3de1ffa66f7a2b629b920de7
Calle1289 <carl.edling@live.se>
2024-02-27 12:45:59 +0100

    ”ye”

--- Project_1/logisti_regression_metod.py
+++ Project_1/logisti_regression_metod.py
@@ -19,45 +19,6 @@ from sklearn.metrics import precision_score
 # Reading data
 df = pd.read_csv('siren_data_train.csv')
 
-## Correlation of features with P-Value, Outliers, Range of Values
-# Correlation of features with P-Value
-results = {}
-for column in df.drop('heard', axis=1).columns:
-    corr_coefficient, p_value = pearsonr(df[column], df['heard'])
-    results[column] = {'Correlation Coefficient': corr_coefficient, 'P-value': p_value}
-results_df = pd.DataFrame(results).T 
-print(results_df)
-
-## Outliers with boxplot
-# Get the list of parameters
-parameters = df.drop(columns=['heard', 'building', 'noise', 'asleep', 'in_vehicle', 'no_windows']).columns # Don't include binary
-
-# Define the number of rows and columns for subplots
-num_rows = 2
-num_cols = 4
-
-# Create a new figure with subplots
-fig, axes = plt.subplots(num_rows, num_cols, figsize=(16, 12))
-
-# Plot boxplots for each parameter
-for i, parameter in enumerate(parameters):
-    row = i // num_cols
-    col = i % num_cols
-    sns.boxplot(y=df[parameter], ax=axes[row, col])
-    axes[row, col].set_title(f'Boxplot of {parameter}')
-    axes[row, col].set_ylabel('Values')
-    axes[row, col].set_xlabel('')
-
-# Hide any unused subplots
-for i in range(len(parameters), num_rows * num_cols):
-    axes[i // num_cols, i % num_cols].axis('off')
-
-# Adjust layout
-plt.tight_layout()
-plt.show()
-
-df.describe() # Range of values
-
 ## Extracting valuable values and leaving some out
 # Calculate the distance and create a new column for it
 df['Distance'] = np.sqrt((df['near_x'] - df['xcoor'])**2 + (df['near_y'] - df['ycoor'])**2)
@@ -126,6 +87,3 @@ std_precision_1 = np.std(precision_1)
 print(f"Nested CV Accuracy: {mean_accuracy:.4f} ± {std_accuracy:.4f}")
 print(f"Nested CV Precision_0: {mean_precision_0:.4f} ± {mean_precision_0:.4f}")
 print(f"Nested CV Precision_1: {mean_precision_1:.4f} ± {mean_precision_1:.4f}")
-
-# x_train_final = scaler.fit_transform(X)
-# optimal_regression_model = LogisticRegression
